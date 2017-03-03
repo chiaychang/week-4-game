bb91fbeaf877775c0f1d86e57db7a701335128f1
Joy Chang <joychang@joyteki-mbp.attlocal.net>
2017-03-02 22:56:11 -0600

    debud-assign new crystal values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,7 +33,7 @@
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+  //Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
     
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
